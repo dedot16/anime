9cc067732a4cd265cc13c755ffba3213e9c096f8
dedot16 <anak.pintar16@gmail.com>
2017-10-07 16:29:05 +0700

    Update linklink.js

--- linklink.js
+++ linklink.js
@@ -25,7 +25,7 @@ if(x=="1"){xxx="/2017/10/common-flex-pcb-issues.html"}
 if(x=="2"){xxx="/2017/10/polyimide-pcb-material-information.html"}
 if(x=="3"){xxx="/2017/10/how-you-can-recycle-printed-circuit.html"}
 if(x=="4"){xxx="/2017/10/the-history-of-printed-circuit-boards.html"}
-if(x=="5"){xxx="/2017/10/why-rushpcb-is-leading-pcb-manufacturer.html"}a_to_ve[i].href="http://www.pcbfree.tk/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
+if(x=="5"){xxx="/2017/10/why-rushpcb-is-leading-pcb-manufacturer.html"}a_to_ve[i].href="http://www.pcbfree.tk"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
